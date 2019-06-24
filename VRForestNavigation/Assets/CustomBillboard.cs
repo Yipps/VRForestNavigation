46ee27430deaadf908162e24652c268272dffe9c
cnazarioga@centro.edu.mx <cnazarioga@centro.edu.mx>
2019-06-23 22:25:05 -0700

    Placement of trees and the zones with modification of the terrain.

---
+++ VRForestNavigation/Assets/CustomBillboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBillboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = Camera.main.transform.position;
+        //Making sure the tree only rotates around the y-axis
+        targetPos.y = transform.position.y;
+        transform.LookAt(targetPos);
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ VRForestNavigation/Assets/Scripts/TreeBillboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBillboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    Vector3 lookAtPosition;
+
+    void Start(){
+        
+        if(player == null){
+        	player =  GameObject.FindWithTag("MainCamera");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+    	lookAtPosition = player.transform.position;
+    	lookAtPosition.y = this.transform.position.y;
+        transform.LookAt(lookAtPosition);
+    }
+}
