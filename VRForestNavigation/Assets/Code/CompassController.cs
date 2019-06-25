0db2ea1bdc9fa9e10d1cb80369df7245bf3d4dae
patrickowens <pat.owens@gmail.com>
2019-06-25 11:50:29 -0700

    Added controller thumbstick events to the compass script to rotate orientation arrow

--- VRForestNavigation/Assets/Code/CompassController.cs
+++ VRForestNavigation/Assets/Code/CompassController.cs
@@ -5,23 +5,28 @@ using VRTK;
 
 public class CompassController : MonoBehaviour
 {
-    public GameObject MagneticNorthArrow;
-    public GameObject OrientationArrow;
+    public GameObject magneticNorthArrow;
+    public GameObject orientationArrow;
     public GameObject up;
 
-    public float MagneticNorthSpeed;
-    public float OrientationArrowSpeed;
+    public float magneticNorthSpeed;
+    public float orientationArrowSpeed;
 
+    private float orientationMovementAmt;
     // Start is called before the first frame update
     void Start()
     {
         //Get VRTK_ControllerEvent and listen to event?
-        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().GripAxisChanged += DoRotateOrientationArrow;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadTouchStart += DoStartCompassMovement;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadTouchEnd += DoStopCompassMovement;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += DoSetOrientationMovementAmount;
     }
 
     private void OnDisable()
     {
-        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().GripAxisChanged -= DoRotateOrientationArrow;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadTouchStart -= DoStartCompassMovement;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadTouchEnd -= DoStopCompassMovement;
+        VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>().TouchpadSenseAxisChanged -= DoSetOrientationMovementAmount;
     }
 
     // Update is called once per frame
@@ -32,13 +37,11 @@ public class CompassController : MonoBehaviour
         if (Input.GetKey("left"))
         {
             print("left");
-            RotateOrientationArrow(-1);
         }
 
         if (Input.GetKey("right"))
         {
             print("right");
-            RotateOrientationArrow(1);
         }
     }
 
@@ -47,26 +50,51 @@ public class CompassController : MonoBehaviour
 
         Vector3 velocity = Vector3.zero;
 
-        Vector3 newRotation = Vector3.SmoothDamp(MagneticNorthArrow.transform.forward, -Vector3.right, ref velocity, MagneticNorthSpeed);
+        Vector3 newRotation = Vector3.SmoothDamp(magneticNorthArrow.transform.forward, -Vector3.right, ref velocity, magneticNorthSpeed);
         //MagneticNorthArrow.transform.localRotation
         Quaternion lookdir = Quaternion.LookRotation(newRotation, transform.up);
 
-        MagneticNorthArrow.transform.rotation = lookdir;
+        magneticNorthArrow.transform.rotation = lookdir;
         
         //MagneticNorthArrow.transform.LookAt(newRotation, transform.parent.up);
         //MagneticNorthArrow.transform.forward = newRotation;
         //MagneticNorthArrow.transform.rotation = Quaternion.LookRotation(newRotation, transform.parent.up);
     }
 
-    private void RotateOrientationArrow(float rotateAmount)
+    IEnumerator CompassMovementCoroutine()
     {
-        OrientationArrow.transform.Rotate(new Vector3(0, OrientationArrowSpeed * rotateAmount, 0));
+        while (true)
+        {
+            orientationArrow.transform.Rotate(new Vector3(0, orientationArrowSpeed * orientationMovementAmt, 0));
+            yield return new WaitForEndOfFrame();
+        }
     }
 
-    private void DoRotateOrientationArrow(object sender, ControllerInteractionEventArgs e)
+    private void DoStartCompassMovement(object sender, ControllerInteractionEventArgs e)
     {
-        print(e.touchpadAxis.x);
-        RotateOrientationArrow(e.touchpadAxis.x);
+        print("Touching");
+        StartCoroutine(CompassMovementCoroutine());
+    }
+
+    private void DoStopCompassMovement(object sender, ControllerInteractionEventArgs e)
+    {
+        print("Stop touching");
+        StopAllCoroutines();
+    }
+
+    private void DoSetOrientationMovementAmount(object sender, ControllerInteractionEventArgs e)
+    {
+        
+        float amt = e.touchpadAxis.x;
+
+        if (Mathf.Abs(amt) < .15)
+        {
+            amt = 0;
+        }
+        print(amt);
+
+        orientationMovementAmt = amt;
+
     }
 
 
