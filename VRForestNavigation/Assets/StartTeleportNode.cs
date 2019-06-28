bb9312e4e61ed9060b211f823b2e442149a47d9f
lawrenceyip1@gmail.com <lawrenceyip1@gmail.com>
2019-06-27 17:56:05 -0700

    Added teleportstart script

---
+++ VRForestNavigation/Assets/StartTeleportNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class StartTeleportNode : MonoBehaviour
+{
+    public LocationNode StartingNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VRTK_HeightAdjustTeleport teleporter = FindObjectOfType<VRTK_HeightAdjustTeleport>();
+
+        teleporter.Teleport(StartingNode.teleportLocation, StartingNode.teleportLocation.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
