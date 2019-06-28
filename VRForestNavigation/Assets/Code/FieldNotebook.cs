884bf9e8419d689110491876b9e86cc00b55854e
lawrenceyip1@gmail.com <lawrenceyip1@gmail.com>
2019-06-28 10:54:03 -0700

    Editting notebook prefab to include page switching.

---
+++ VRForestNavigation/Assets/Code/FieldNotebook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class FieldNotebook : MonoBehaviour
+{
+    Animator anim;
+    private int currentPage = 0;
+    public int numOfPages = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        VRTK_SDKManager.instance.scriptAliasLeftController.GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += DoChangePage;
+    }
+
+    private void OnDisable()
+    {
+        VRTK_SDKManager.instance.scriptAliasLeftController.GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged -= DoChangePage;
+    }
+
+    private void DoChangePage(object sender, ControllerInteractionEventArgs e)
+    {
+        int pageChangeIndex = 1;
+
+        if(e.touchpadAxis.x < 0)
+        {
+            pageChangeIndex = -1;
+        }
+
+        ChangePage(pageChangeIndex);
+    }
+
+    private void ChangePage(float pageChange)
+    {
+        currentPage = (int) Mathf.Clamp(currentPage + pageChange, 0, numOfPages);
+        anim.SetInteger("CurrentPage", currentPage);
+    }
+}
